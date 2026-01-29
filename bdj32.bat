700a13e422c3b0d7baf75d545129c286d698cd65
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2026-01-29 18:48:19 +0100

    add DJGPP 32-bit build script

---
+++ bdj32.bat
@@ -0,0 +1,17 @@
+set CFLAGS=-march=i386 -O1 -flto -fwhole-program -Wpedantic -Wall -Wextra -Wno-attributes
+
+@set GLOBOBJS=
+@set GLOBOBJS=%GLOBOBJS% engine.c
+@set GLOBOBJS=%GLOBOBJS% engine2.c
+@set GLOBOBJS=%GLOBOBJS% filesystem.c
+@set GLOBOBJS=%GLOBOBJS% global.c
+@set GLOBOBJS=%GLOBOBJS% p_setup.c
+@set GLOBOBJS=%GLOBOBJS% palette.c
+@set GLOBOBJS=%GLOBOBJS% tables.c
+@set GLOBOBJS=%GLOBOBJS% test.c
+@set GLOBOBJS=%GLOBOBJS% tiles.c
+@set GLOBOBJS=%GLOBOBJS% z_zone.c
+
+gcc %GLOBOBJS% %CFLAGS% -o release/dj32test.exe
+strip -s release/dj32test.exe
+stubedit release/dj32test.exe dpmi=CWSDPR0.EXE
