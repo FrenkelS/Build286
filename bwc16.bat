bab71705de5b4a48585cca07a4e3b63c3803b24e
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2026-01-29 18:46:32 +0100

    add Watcom 16-bit build script

---
+++ bwc16.bat
@@ -0,0 +1,18 @@
+set CFLAGS=-2 -os -bcl=dos -ml -k4096 -q -wx -DC_ONLY
+
+@set GLOBOBJS=
+@set GLOBOBJS=%GLOBOBJS% engine.c
+@set GLOBOBJS=%GLOBOBJS% engine2.c
+@set GLOBOBJS=%GLOBOBJS% filesystem.c
+@set GLOBOBJS=%GLOBOBJS% global.c
+@set GLOBOBJS=%GLOBOBJS% p_setup.c
+@set GLOBOBJS=%GLOBOBJS% palette.c
+@set GLOBOBJS=%GLOBOBJS% tables.c
+@set GLOBOBJS=%GLOBOBJS% test.c
+@set GLOBOBJS=%GLOBOBJS% tiles.c
+@set GLOBOBJS=%GLOBOBJS% z_zone.c
+
+wcl %GLOBOBJS% %CFLAGS% -fe=release/wc16test.exe -fm=release/wc16test.map
+
+del *.err
+del *.obj
